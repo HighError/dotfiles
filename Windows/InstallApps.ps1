f836b6972a8efa18bc1b93c9c0aaf0d716a224db
HighError <46526245+HighError@users.noreply.github.com>
2022-09-10 18:44:15 +0300

    InstallApps

---
+++ Windows/InstallApps.ps1
@@ -0,0 +1,48 @@
+# Microsoft Visual C++ Redistributable
+winget install -e --id Microsoft.VC++2005Redist-x86 # Microsoft Visual C++ 2005 Redistributable x86
+winget install -e --id Microsoft.VC++2005Redist-x64 # Microsoft Visual C++ 2005 Redistributable x64
+
+winget install -e --id Microsoft.VC++2008Redist-x86 # Microsoft Visual C++ 2008 Redistributable x86
+winget install -e --id Microsoft.VC++2008Redist-x64 # Microsoft Visual C++ 2008 Redistributable x64
+
+winget install -e --id Microsoft.VC++2010Redist-x86 # Microsoft Visual C++ 2010 Redistributable x86
+winget install -e --id Microsoft.VC++2010Redist-x64 # Microsoft Visual C++ 2010 Redistributable x64
+
+winget install -e --id Microsoft.VC++2012Redist-x86 # Microsoft Visual C++ 2012 Redistributable x86
+winget install -e --id Microsoft.VC++2012Redist-x64 # Microsoft Visual C++ 2012 Redistributable x64
+
+winget install -e --id Microsoft.VC++2015-2022Redist-x86 # Microsoft Visual C++ 2015-2022 Redistributable x86
+winget install -e --id Microsoft.VC++2015-2022Redist-x64 # Microsoft Visual C++ 2015-2022 Redistributable x64
+
+# Base apps
+winget install -e --id Google.Chrome # Google Chrome
+winget install -e --id Discord.Discord # Discord
+winget install -e --id Telegram.TelegramDesktop # Telegram
+winget install -e --id 7zip.7zip # 7-zip
+winget install -e --id ShareX.ShareX # ShareX
+winget install -e --id clsid2.mpc-hc # MPC-HC
+
+# Devs
+winget install -e --id Git.Git # Git
+winget install -e --id OpenJS.NodeJS # Node.JS
+winget install -e --id Atlassian.Sourcetree # SourceTree
+winget install -e --id Microsoft.VisualStudioCode # VSCode
+
+# Terminal 
+winget install -e --id Microsoft.PowerShell #PowerShell 7+
+winget install -e --id JanDeDobbeleer.OhMyPosh # OhMyPosh
+winget install -e --id gerardog.gsudo # gsudo
+
+# Games
+winget install -e --id Valve.Steam #Steam
+winget install -e --id EpicGames.EpicGamesLauncher # Epic Games Launcher
+winget install -e --id Playnite.Playnite # Playnite
+
+# Valorant addons
+winget install -e --id Soneliem.WAIUA # WAIUA
+
+# Other
+winget install -e --id Microsoft.PowerToys # PowerToys
+winget install -e --id qBittorrent.qBittorrent # qBittorrent
+winget install -e --id Spotify.Spotify # Spotify
+
