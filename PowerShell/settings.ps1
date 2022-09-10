1fd5cac4f4ccb8118919ea3b5262f8e36306f44b
HighError <46526245+HighError@users.noreply.github.com>
2022-09-10 18:36:11 +0300

    PowerShell

---
+++ PowerShell/settings.ps1
@@ -0,0 +1,7 @@
+# Need install oh-my-posh and terminal-icons
+# For install oh-my-posh: winget install JanDeDobbeleer.OhMyPosh -s winget
+# For install terminal-icons: Install-Module -Name Terminal-Icons -Repository PSGallery
+
+oh-my-posh init pwsh | Invoke-Expression
+Import-Module -Name Terminal-Icons
+oh-my-posh init pwsh --config C:\Users\HighError\AppData\Local\Programs\oh-my-posh\themes\velvet.omp.json | Invoke-Expression
