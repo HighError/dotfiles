843f9160c3fcb62a1db1cfbc4b9fcbf16ae432ab
HighError <46526245+HighError@users.noreply.github.com>
2022-09-10 19:01:10 +0300

    Update InstallApps.ps1

--- Windows/InstallApps.ps1
+++ Windows/InstallApps.ps1
@@ -45,4 +45,5 @@ winget install -e --id Soneliem.WAIUA # WAIUA
 winget install -e --id Microsoft.PowerToys # PowerToys
 winget install -e --id qBittorrent.qBittorrent # qBittorrent
 winget install -e --id Spotify.Spotify # Spotify
+winget install -e --id Azul.Zulu.17.JDK # JDK
 
