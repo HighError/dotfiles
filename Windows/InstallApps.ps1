51cabb68c377c36800ee09ab0aca666a052f5327
HighError <46526245+HighError@users.noreply.github.com>
2022-12-30 15:50:18 +0200

    Update InstallApps.ps1

--- Windows/InstallApps.ps1
+++ Windows/InstallApps.ps1
@@ -1,21 +1,21 @@
 # Microsoft Visual C++ Redistributable
-winget install -e --id Microsoft.VC++2005Redist-x86 # Microsoft Visual C++ 2005 Redistributable x86
-winget install -e --id Microsoft.VC++2005Redist-x64 # Microsoft Visual C++ 2005 Redistributable x64
+winget install -e --id Microsoft.VCRedist.2005.x86 # Microsoft Visual C++ 2005 Redistributable x86
+winget install -e --id Microsoft.VCRedist.2005.x64 # Microsoft Visual C++ 2005 Redistributable x64
 
-winget install -e --id Microsoft.VC++2008Redist-x86 # Microsoft Visual C++ 2008 Redistributable x86
-winget install -e --id Microsoft.VC++2008Redist-x64 # Microsoft Visual C++ 2008 Redistributable x64
+winget install -e --id Microsoft.VCRedist.2008.x86 # Microsoft Visual C++ 2008 Redistributable x86
+winget install -e --id Microsoft.VCRedist.2008.x64 # Microsoft Visual C++ 2008 Redistributable x64
 
-winget install -e --id Microsoft.VC++2010Redist-x86 # Microsoft Visual C++ 2010 Redistributable x86
-winget install -e --id Microsoft.VC++2010Redist-x64 # Microsoft Visual C++ 2010 Redistributable x64
+winget install -e --id Microsoft.VCRedist.2010.x86 # Microsoft Visual C++ 2010 Redistributable x86
+winget install -e --id Microsoft.VCRedist.2010.x64 # Microsoft Visual C++ 2010 Redistributable x64
 
-winget install -e --id Microsoft.VC++2012Redist-x86 # Microsoft Visual C++ 2012 Redistributable x86
-winget install -e --id Microsoft.VC++2012Redist-x64 # Microsoft Visual C++ 2012 Redistributable x64
+winget install -e --id Microsoft.VCRedist.2012.x86 # Microsoft Visual C++ 2012 Redistributable x86
+winget install -e --id Microsoft.VCRedist.2012.x64 # Microsoft Visual C++ 2012 Redistributable x64
 
-winget install -e --id Microsoft.VC++2015-2022Redist-x86 # Microsoft Visual C++ 2015-2022 Redistributable x86
-winget install -e --id Microsoft.VC++2015-2022Redist-x64 # Microsoft Visual C++ 2015-2022 Redistributable x64
+winget install -e --id Microsoft.VCRedist.2015+.x86 # Microsoft Visual C++ 2015-2022 Redistributable x86
+winget install -e --id Microsoft.VCRedist.2015+.x64 # Microsoft Visual C++ 2015-2022 Redistributable x64
 
 # Base apps
-winget install -e --id Google.Chrome # Google Chrome
+winget install -e --id Brave.Brave # Brave
 winget install -e --id Discord.Discord # Discord
 winget install -e --id Telegram.TelegramDesktop # Telegram
 winget install -e --id 7zip.7zip # 7-zip
@@ -26,9 +26,10 @@ winget install -e --id VideoLAN.VLC # VLC
 winget install -e --id Git.Git # Git
 winget install -e --id OpenJS.NodeJS # Node.JS
 winget install -e --id Yarn.Yarn # Yarn
-winget install -e --id Atlassian.Sourcetree # SourceTree
 winget install -e --id Microsoft.VisualStudioCode # VSCode
 winget install -e --id Docker.DockerDesktop # Docker Desktop
+winget install -e --id MongoDB.Compass.Full # MongoDB Compass
+winget install -e --id Insomnia.Insomnia # Insomnia
 
 # Terminal 
 winget install -e --id Microsoft.PowerShell #PowerShell 7+
@@ -47,6 +48,9 @@ winget install -e --id Soneliem.WAIUA # WAIUA
 winget install -e --id Microsoft.PowerToys # PowerToys
 winget install -e --id qBittorrent.qBittorrent # qBittorrent
 winget install -e --id Spotify.Spotify --force # Spotify
-winget install -e --id Azul.Zulu.17.JDK # JDK
+winget install -e --id BellSoft.LibericaJDK.19.Full # JDK
 winget install -e --id File-New-Project.EarTrumpet # EarTrumpet
-winget install -e --id OpenVPNTechnologies.OpenVPN #OpenVPN
+winget install -e --id OpenVPNTechnologies.OpenVPN # OpenVPN
+winget install -e --id Bitwarden.Bitwarden # BitWarden
+winget install -e --id Obsidian.Obsidian # Obsidian
+winget install -e --id Nvidia.GeForceExperience # GeForceExperience
