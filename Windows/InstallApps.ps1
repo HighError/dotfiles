83610f4d95622de93601f4767746b6e372db0b23
HighError <46526245+HighError@users.noreply.github.com>
2023-02-24 00:32:26 +0200

    Update InstallApps.ps1

--- Windows/InstallApps.ps1
+++ Windows/InstallApps.ps1
@@ -1,55 +1,49 @@
 # Microsoft Visual C++ Redistributable
-winget install -e --id Microsoft.VCRedist.2005.x86 # Microsoft Visual C++ 2005 Redistributable x86
-winget install -e --id Microsoft.VCRedist.2005.x64 # Microsoft Visual C++ 2005 Redistributable x64
+winget install --id Microsoft.VCRedist.2005.x86 # Microsoft Visual C++ 2005 Redistributable x86
+winget install --id Microsoft.VCRedist.2005.x64 # Microsoft Visual C++ 2005 Redistributable x64
 
-winget install -e --id Microsoft.VCRedist.2008.x86 # Microsoft Visual C++ 2008 Redistributable x86
-winget install -e --id Microsoft.VCRedist.2008.x64 # Microsoft Visual C++ 2008 Redistributable x64
+winget install --id Microsoft.VCRedist.2008.x86 # Microsoft Visual C++ 2008 Redistributable x86
+winget install --id Microsoft.VCRedist.2008.x64 # Microsoft Visual C++ 2008 Redistributable x64
 
-winget install -e --id Microsoft.VCRedist.2010.x86 # Microsoft Visual C++ 2010 Redistributable x86
-winget install -e --id Microsoft.VCRedist.2010.x64 # Microsoft Visual C++ 2010 Redistributable x64
+winget install --id Microsoft.VCRedist.2010.x86 # Microsoft Visual C++ 2010 Redistributable x86
+winget install --id Microsoft.VCRedist.2010.x64 # Microsoft Visual C++ 2010 Redistributable x64
 
-winget install -e --id Microsoft.VCRedist.2012.x86 # Microsoft Visual C++ 2012 Redistributable x86
-winget install -e --id Microsoft.VCRedist.2012.x64 # Microsoft Visual C++ 2012 Redistributable x64
+winget install --id Microsoft.VCRedist.2012.x86 # Microsoft Visual C++ 2012 Redistributable x86
+winget install --id Microsoft.VCRedist.2012.x64 # Microsoft Visual C++ 2012 Redistributable x64
 
-winget install -e --id Microsoft.VCRedist.2015+.x86 # Microsoft Visual C++ 2015-2022 Redistributable x86
-winget install -e --id Microsoft.VCRedist.2015+.x64 # Microsoft Visual C++ 2015-2022 Redistributable x64
+winget install --id Microsoft.VCRedist.2015+.x86 # Microsoft Visual C++ 2015-2022 Redistributable x86
+winget install --id Microsoft.VCRedist.2015+.x64 # Microsoft Visual C++ 2015-2022 Redistributable x64
 
 # Base apps
-winget install -e --id Google.Chrome # Google Chrome
-winget install -e --id Discord.Discord # Discord
-winget install -e --id Telegram.TelegramDesktop # Telegram
-winget install -e --id 7zip.7zip # 7-zip
-winget install -e --id ShareX.ShareX # ShareX
-winget install -e --id VideoLAN.VLC # VLC
+winget install --id Google.Chrome # Google Chrome
+winget install --id Discord.Discord # Discord
+winget install --id Telegram.TelegramDesktop # Telegram
+winget install --id M2Team.NanaZip # 7-zip alternative (comp. with Windows 11 context menu)
+winget install --id ShareX.ShareX # ShareX
 
 # Devs
-winget install -e --id Git.Git # Git
-winget install -e --id OpenJS.NodeJS # Node.JS
-winget install -e --id Yarn.Yarn # Yarn
-winget install -e --id Microsoft.VisualStudioCode # VSCode
-winget install -e --id Docker.DockerDesktop # Docker Desktop
-winget install -e --id MongoDB.Compass.Full # MongoDB Compass
-winget install -e --id Insomnia.Insomnia # Insomnia
+winget install --id Git.Git # Git
+winget install --id OpenJS.NodeJS # Node.JS
+winget install --id Microsoft.VisualStudioCode # VSCode
+winget install --id Docker.DockerDesktop # Docker Desktop
+winget install --id MongoDB.Compass.Full # MongoDB Compass
+winget install --id Insomnia.Insomnia # Insomnia
 
 # Terminal 
-winget install -e --id Microsoft.PowerShell #PowerShell 7+
-winget install -e --id JanDeDobbeleer.OhMyPosh # OhMyPosh
-winget install -e --id gerardog.gsudo # gsudo
+winget install --id Microsoft.PowerShell #PowerShell 7+
+winget install --id JanDeDobbeleer.OhMyPosh # OhMyPosh
+winget install --id gerardog.gsudo # gsudo
 
 # Games Launchers
-winget install -e --id Valve.Steam #Steam
-winget install -e --id EpicGames.EpicGamesLauncher # Epic Games Launcher
-winget install -e --id Playnite.Playnite # Playnite
-
-# For Gaming
-winget install -e --id Soneliem.WAIUA # WAIUA
+winget install --id Valve.Steam #Steam
+winget install --id EpicGames.EpicGamesLauncher # Epic Games Launcher
+winget install --id Playnite.Playnite # Playnite
 
 # Other
-winget install -e --id Microsoft.PowerToys # PowerToys
-winget install -e --id qBittorrent.qBittorrent # qBittorrent
-winget install -e --id Spotify.Spotify --force # Spotify
-winget install -e --id BellSoft.LibericaJDK.19.Full # JDK
-winget install -e --id File-New-Project.EarTrumpet # EarTrumpet
-winget install -e --id OpenVPNTechnologies.OpenVPN # OpenVPN
-winget install -e --id Obsidian.Obsidian # Obsidian
-winget install -e --id Nvidia.GeForceExperience # GeForceExperience
+winget install --id Microsoft.PowerToys # PowerToys
+winget install --id qBittorrent.qBittorrent # qBittorrent
+winget install --id Spotify.Spotify --force # Spotify
+winget install --id Spicetify.Spicetify # Spicetify (Spotify mod)
+winget install --id BellSoft.LibericaJDK.19.Full # JDK
+winget install --id OpenVPNTechnologies.OpenVPN # OpenVPN
+winget install --id Obsidian.Obsidian # Obsidian
