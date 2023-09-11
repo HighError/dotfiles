18764cb1301b3737c966e5361e3375a2e55cd705
HighError <46526245+HighError@users.noreply.github.com>
2023-09-11 23:31:34 +0300

    Update InstallApps.ps1

--- Windows/InstallApps.ps1
+++ Windows/InstallApps.ps1
@@ -22,12 +22,13 @@ winget install --id M2Team.NanaZip # 7-zip alternative (comp. with Windows 11 co
 winget install --id ShareX.ShareX # ShareX
 
 # Devs
-winget install --id Git.Git # Git
-winget install --id OpenJS.NodeJS # Node.JS
-winget install --id Microsoft.VisualStudioCode # VSCode
+winget install --id Microsoft.VisualStudioCode -i # VSCode
+winget install --id Git.Git -i # Git
+winget install --id OpenJS.NodeJS.LTS # Node.JS
 winget install --id Docker.DockerDesktop # Docker Desktop
 winget install --id MongoDB.Compass.Full # MongoDB Compass
 winget install --id Insomnia.Insomnia # Insomnia
+winget install --id JetBrains.WebStorm # WebStorm
 
 # Terminal 
 winget install --id Microsoft.PowerShell #PowerShell 7+
@@ -36,13 +37,13 @@ winget install --id gerardog.gsudo # gsudo
 
 # Games Launchers
 winget install --id Valve.Steam #Steam
-winget install --id EpicGames.EpicGamesLauncher # Epic Games Launcher
 winget install --id Playnite.Playnite # Playnite
 
 # Other
 winget install --id qBittorrent.qBittorrent # qBittorrent
-winget install --id Spotify.Spotify --force # Spotify
+winget install --id Spotify.Spotify # Spotify
 winget install --id Obsidian.Obsidian # Obsidian
 winget install --id nilesoft.shell # NileSoft Shell
 winget install --id Flow-Launcher.Flow-Launcher # Flow lanhcer
 winget install --id OBSProject.OBSStudio # OBS
+winget install --id Twilio.Authy #Authy
