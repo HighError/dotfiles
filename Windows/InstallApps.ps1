dacd1887ed140b2a1a97930bfaefc4cc77b073c8
HighError <46526245+HighError@users.noreply.github.com>
2024-07-28 14:51:48 +0300

    Update InstallApps.ps1

--- Windows/InstallApps.ps1
+++ Windows/InstallApps.ps1
@@ -3,15 +3,15 @@ Install-Module -Name VcRedist
 
 # Base apps
 winget install --id Google.Chrome # Google Chrome
-winget install --id 7Zip.7Zip # 7-zip
-winget install --id ShareX.ShareX # ShareX
+winget install --id M2Team.NanaZip # NanaZip
+winget install --id ShareX.ShareX -i # ShareX
 
 # Socials
 winget install --id Discord.Discord # Discord
 winget install --id Telegram.TelegramDesktop # Telegram
 
 # Devs
-winget install --id VSCodium.VSCodium -i # VSCode
+winget install --id Microsoft.VisualStudioCode -i # VSCode
 winget install --id Git.Git -i # Git
 winget install --id OpenJS.NodeJS.LTS # Node.JS
 winget install --id Docker.DockerDesktop # Docker Desktop
@@ -25,16 +25,13 @@ Install-Module -Name Terminal-Icons # Terminal Icons
 
 # Games Launchers
 winget install --id Valve.Steam #Steam
-winget install --id Playnite.Playnite # Playnite
 winget install --id derrod.legendary # EGS (Legendary)
+winget install --id Playnite.Playnite # Playnite
 
 # Other
 winget install --id qBittorrent.qBittorrent # qBittorrent
 winget install --id Spotify.Spotify # Spotify
 winget install --id nilesoft.shell # NileSoft Shell
 winget install --id OBSProject.OBSStudio # OBS
-winget install --id Flow-Launcher.Flow-Launcher # Flow Launcher
-winget install --id AppFlowy.AppFlowy # AppFlowy
-
-winget install --id Gyan.FFmpeg # FFmpeg
-winget install --id yt-dlp.yt-dlp # Youtube-DLP
+winget install --id Notion.Notion # NotionVideoLAN.VLC
+winget install --id VideoLAN.VLC # VLC
